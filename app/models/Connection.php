4a795dabfd1e9e0e87fd7f995652a98312e0688d
salvatoreDeploy <70715761+salvatoreDeploy@users.noreply.github.com>
2022-01-08 21:16:01 -0300

    Update Connection.php

--- app/models/Connection.php
+++ app/models/Connection.php
@@ -17,7 +17,7 @@ class Connection
      */
     public static function connect()
     {
-        $pdo = new PDO("mysql:host=localhost;dbname=twig_slim;charset=utf8", "root", "");
+        $pdo = new PDO("mysql:host=localhost;dbname=DBNAME;charset=utf8", "USUARIO", "SENHA");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
